4424083186ca06b52ccb5b06fc5036aacaab5972
teman67 <>
2023-11-27 17:38:39 +0000

    add descriptions to course lists

--- courses/urls.py
+++ courses/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index, list_courses
+from .views import index, list_courses, your_view
 
 
 urlpatterns = [
     path('', index.as_view(),name='home'),
     path('courses/', list_courses, name='courses'),
+    path('your_view/', your_view, name='your_view'),
 ]
--- courses/views.py
+++ courses/views.py
@@ -9,3 +9,21 @@ class index(TemplateView):
 def list_courses(request):
     
     return render(request, 'list_courses.html')
+
+
+def your_view(request):
+    descriptions = {
+        "0": "Description of Course 0",
+        "1": "Description of Course 1",
+        "2": "Description of Course 2",
+        "3": "Description of Course 3",
+        "4": "Description of Course 4",
+        "5": "Description of Course 5",
+        "6": "Description of Course 6",
+        "7": "Description of Course 7",
+        "8": "Description of Course 8",
+        "9": "Description of Course 9",
+    }
+
+    context = {'descriptions': descriptions}
+    return render(request, 'list_courses.html', context)
