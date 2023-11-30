5195172cc412fd749cab81aefef34195d0dbe00f
teman67 <>
2023-11-30 17:33:53 +0000

    add course_detail.html

--- courses/urls.py
+++ courses/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import index, list_courses, BookingView
+from .views import index, list_courses, BookingView, course_detail
 
 
 urlpatterns = [
     path('', index.as_view(),name='home'),
     path('courses/', list_courses, name='courses'),
     path('booking/', BookingView.as_view(), name='booking'),
-
+    path('course/<int:course_id>/', course_detail, name='course_detail'),
 ]
--- courses/views.py
+++ courses/views.py
@@ -21,3 +21,19 @@ class BookingView(View):
         # Your logic for handling the POST request for the booking page
         # This is optional, add it if your booking page handles form submissions
         pass
+
+
+def course_detail(request, course_id):
+    # Assuming you have a list of descriptions, replace this with your actual data source
+    descriptions = ["Description for course 1", "Description for course 2", "3","4","5","6","7","8","9","10",]
+
+    # Ensure course_id is a valid index
+    if 0 <= course_id < len(descriptions):
+        context = {
+            'course_id': course_id,
+            'description': descriptions[course_id],
+        }
+        return render(request, 'course_detail.html', context)
+    else:
+        # Handle invalid course_id (e.g., redirect to an error page)
+        return HttpResponseNotFound("Course not found")
