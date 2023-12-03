41f5442688b8efe7306421d6162022e6b340de1f
teman67 <>
2023-12-03 21:43:02 +0000

    link the course_detail to database

--- courses/models.py
+++ courses/models.py
@@ -6,6 +6,8 @@ class Course(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     # image_link = models.URLField(null=True)
+    def get_course_ids(self):
+        return list(Course.objects.values_list('id', flat=True))
 
     def __str__(self):
         return self.name
--- courses/urls.py
+++ courses/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', index.as_view(),name='home'),
     # path('courses/', list_courses, name='courses'),
     path('booking/', BookingView.as_view(), name='booking'),
-    path('course/<int:course_id>/', course_detail, name='course_detail'),
     path('user_profile/', user_profile, name='user_profile'),
     path('courses/', course_list, name='course_list'),
+    path('course/<int:course_id>/', course_detail, name='course_detail'),
 ]
